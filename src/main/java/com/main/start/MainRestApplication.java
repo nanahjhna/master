ad86c9468aa9e6f0c5f47ae0ad6ac3a5c73d3ffd
nanahjhna <nanahjhna7@naver.com>
2021-10-24 05:03:57 +0900

    first commit

---
+++ src/main/java/com/main/start/MainRestApplication.java
@@ -0,0 +1,13 @@
+package com.main.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainRestApplication.class, args);
+	}
+
+}
